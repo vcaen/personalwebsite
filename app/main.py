467ab1446e6274c058629d8aa2543c918752d1fc
Vadim Caen <vadimcaen@gmail.com>
2015-08-16 20:53:33 +0200

    added pdf

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from settings import APP_STATIC
+from flask import Flask, render_template, send_from_directory
 from app.controller import cv as cv_controller
 
 
@@ -10,6 +11,10 @@ app = Flask(__name__)
 @app.route('/')
 def show_about():
     return cv_controller.CVController().renderCVToHTML()
+
+@app.route('/cv.pdf')
+def get_pdf_cv():
+    return send_from_directory(APP_STATIC, 'file/cv.pdf')
 
 if __name__ == '__main__':
     app.run()
