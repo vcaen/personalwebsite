f007e149f4404cc87251df201d6f4faa41a77ddd
Vadim Caen <vadimcaen@gmail.com>
2015-12-18 17:01:16 +0100

    button

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -4,12 +4,14 @@
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
 
-        $('.scrollTo').click(function () { // Au clic sur un élément
-            console.log('click')
-            var page = $('.education').first() // Page cible
-            var speed = 750; // Durée de l'animation (en ms)
-            $('html, body').animate({scrollTop: $(page).offset().top}, speed); // Go
-            return false;
+        $('.scrollTo').each(function (i, e) {
+            $(e).find('a').click(function (event) { // Au clic sur un élément
+                event.preventDefault();
+                var page = $("." + $(this).attr('href')); // Page cible
+                var speed = 750; // Durée de l'animation (en ms)
+                $('html, body').animate({scrollTop: $(page).offset().top}, speed); // Go
+                ;
+            });
         });
 
     }); // end of document ready
