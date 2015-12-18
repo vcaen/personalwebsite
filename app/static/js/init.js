0704f5d37978d0fdcef2fbdd649f94549e17471c
Vadim Caen <vadimcaen@gmail.com>
2015-12-18 12:28:25 +0100

    description

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -1,10 +1,16 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
 
-    })
+        $('.scrollTo').click(function () { // Au clic sur un élément
+            console.log('click')
+            var page = $('.education').first() // Page cible
+            var speed = 750; // Durée de l'animation (en ms)
+            $('html, body').animate({scrollTop: $(page).offset().top}, speed); // Go
+            return false;
+        });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    }); // end of document ready
+})(jQuery) // end of jQuery name space
